12c2289becabfcc1dbaac9aec23b7001605b8201
Swiss <154095438+swissthree@users.noreply.github.com>
2026-09-24 23:35:15 -0600

    Add files via upload

---
+++ cs50p/problem_set0/einstein.py
@@ -0,0 +1,14 @@
+#Marcus Ee
+#einstein
+#Replaces text with specified values
+
+option = input("Enter a whole number between 1 and 3: ")
+match option:
+    case "1": 
+        print("90000000000000000")
+    case "2":
+        print("1260000000000000000")
+    case "3":
+        print("4500000000000000000")
+    case _:
+        print("Invalid input, please lsiten to instructions!")
