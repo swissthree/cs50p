66a26d4d811c1269fab7e3b68aaa40b31c5160e3
Swiss <154095438+swissthree@users.noreply.github.com>
2026-09-25 00:05:51 -0600

    Delete cs50p directory

--- cs50p/problem_set0/indoor_voice.py
+++
@@ -1,6 +0,0 @@
-#Marcus Ee
-#indoor_voice
-#Turns all texts to lowercase
-
-text = input("Try Yelling: ").casefold()
-print(text)
--- cs50p/problem_set0/making_faces.py
+++
@@ -1,6 +0,0 @@
-#Marcus Ee
-#making_faces
-#Turns :) and :( into emojis
-
-text = input("Enter a message: ").replace(":)", "🙂")
-print(text.replace(":(", "🙁"))
