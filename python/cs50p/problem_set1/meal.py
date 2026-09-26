c2f9b45704d3316cee54373e9196125f1b321e61
swissthree <marcusee2008@gmail.com>
2026-09-25 18:45:54 -0600

    finished meal.py

--- python/cs50p/problem_set1/meal.py
+++ python/cs50p/problem_set1/meal.py
@@ -3,12 +3,25 @@
 #user inputs a time and comupter judges wheither it's meal time or not
 
 def main():
-    time = input("What time is it? ").split()
+    time = input("What time is it? ").split(":")
+    try: 
+        if time[1].split("p")[1] == "m":
+            convertTime(int(time[0]))
+    except IndexError:    
+        checkMealTime(int(time[0]))
 
-
-def convert(time):
-    ...
-
+def convertTime(time):
+    checkMealTime(time + 12)
+    
+def checkMealTime(hour):
+    if hour >= 7 and hour <= 8:
+        print("Breakfast time!")
+    elif hour >= 12 and hour <= 13:
+        print("Lunch time!")
+    elif hour >= 18 and hour <= 19:
+        print("Dinner time!") 
+    else:
+        print("Not meal time!")
 #loop
 if __name__ == "__main__":
     main()
